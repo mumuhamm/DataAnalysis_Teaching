5cfb6acac4d75089474e3b80d48b32e73fe4cf42
muhammad.muhammad.alibordi@cern.ch <muhammad.muhammad.alibordi@cern.ch>
2022-02-15 10:44:09 +0530

    emission disk around a Schwarzschild Black Hole

---
+++ MC_simulation/emission_disk.py
@@ -0,0 +1,16 @@
+import astropy.units as u
+from einsteinpy.rays import Shadow
+from einsteinpy.plotting import ShadowPlotter
+
+
+mass = 1 * u.kg
+fov = 30 * u.km
+shadow = Shadow(mass=mass, fov=fov, n_rays=1000)
+obj = ShadowPlotter(shadow=shadow, is_line_plot=True)
+obj.plot()
+obj.show()
+
+obj = ShadowPlotter(shadow=shadow, is_line_plot=False)
+obj.plot()
+obj.show()
+
